92097268dacfafd6dfea4baee58bff199403441d
TheEsus <kostya.antonev.99@mail.ru>
2018-06-15 18:41:37 +0700

    TheEsus
    Fix graphica.cpp

--- src/graphica.cpp
+++ src/graphica.cpp
@@ -3,12 +3,26 @@
 #include <conio.h>
 #include <graphics.h>
 
-void DrawStdWin(vforms *v, stdco *t){
-	 
+void far settextstyle(int f,int b,int ch);  
+
+void DrawStdWin(vforms *v, stdco *t) {
+
+	outtextxy(t[0].x,t[0].y,v[0].form1);
+	outtextxy(t[1].x,t[1].y,v[0].form2);
+	outtextxy(t[2].x,t[2].y,v[0].form3);
+    far settextstyle(4,0,1);
+	outtextxy(650,670,"Anton'ev Konstantin");
+	outtextxy(750,700,"&");
+	outtextxy(800,730,"Ilya Kazancev");
+	outtextxy(610,730,"Igor Lipovtsev");
+	far settextstyle(4,0,3);
+	outtextxy(10,735,"SibSUTIS, 2018");
 }
 
-void PutStr(){
+void PutStr() {
 	
+	
+
 }
 
 
